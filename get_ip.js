84867dbeb1261b683dd94487d6aa7291b463e425
k239i <sueqk@outlook.jp>
2021-03-25 20:19:09 +0900

    うんこ、

---
+++ get_ip.js
@@ -0,0 +1,23 @@
+const http = require('http');
+function getip(host){
+  http.get({
+    hostname: host.hostname,
+    port: host.port,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36'
+    }
+  },
+    function(res){
+      var ip = res.socket.remoteAddress;
+      console.log(ip)
+    }
+  )
+}
+process.on('message', message => {
+  getip(message);
+})
+
+process.on('uncaughtException', error => {
+  process.send(error)
+  process.exit(1)
+})
---
+++ index.js
@@ -0,0 +1,26 @@
+const cp = require('child_process');
+const discord = require('discord.js');
+const url = require('url');
+const client = new discord.Client();
+client.on('message', m => {
+  if(m.author.bot);
+  if(!m.mentions.has(client.user, { ignoreEveryone: true, ignoreRoles: true})) return;
+  const _url = m.content.replace(/(<@!?\d+>\s?)+/,'');
+  var host = new url.parse(_url);
+  if(!m.content.match(/https?:\/\//)) host = {
+    hostname: _url
+  };
+  const sbp = cp.fork(__dirname + '/get_ip.js');
+  sbp.send(host);
+  sbp.stdout.on("data", (data) => {
+    data = data.toString();
+    m.channel.send(data)
+  });
+  setTimeout(() => sbp.kill(0), 5000)
+  sbp.on('message', message => {
+    m.reply('CheckHost '+message, {code: true})
+  })
+  sbp.on("exit", (code) => console.log('sbp: exited code '+code));
+});
+client.on('ready',() => console.log('ready'))
+client.login('とけん、ひあｗｗｗｗｗｗｗｗ');
