40fc6a226cbe503703fb8b430ad5a1eed38e7786
sueqk <68449029+k239i@users.noreply.github.com>
2021-03-26 06:50:42 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,17 +6,27 @@ client.on('message', async m => {
   const message = m;
   if(m.author.bot);
   if(!m.mentions.has(client.user, { ignoreEveryone: true, ignoreRoles: true})) return;
-  const _url = m.content.replace(/(<@!?\d+>\s?)+/,'');
+  var _url = m.content.replace(/(<@!?\d+>\s?)+/,'');
   var host = new url.parse(_url);
-  if(!m.content.match(/https?:\/\//)) host = {
-    hostname: _url
+  if(!m.content.match(/https?:\/\//)){
+    var vaa = _url.split(':');
+    if(vaa[1]){
+      host = {
+        hostname: vaa[0],
+        port: Number(vaa[1])
+      }
+    }else{
+      host = {
+        hostname: _url,
+      }
+    }
   };
   try{
     const _get = await getip(host);
-    message.channel.send(String(_get)+'\n?');
+    message.channel.send(String(_get)+'\n');
     checked += 1;
   }catch(e){
-    message.reply(String(e)+'\n?',{code: true});
+    message.reply(String(e)+'\n',{code: true});
   };
 });
 client.on('ready',() => {
@@ -33,17 +43,21 @@ client.login(process.env.token); //token入れないと死にます
 const http = require('http');
 function getip(host){
   return new Promise((resolve, reject) => {
-    http.get({
-      hostname: host.hostname,
-      port: host.port,
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36'
-      }
-    },
-      function(res){
-        var ip = res.socket.remoteAddress;
-        resolve(ip);
-      }
-    )
+    try{
+      http.get({
+        hostname: host.hostname,
+        port: host.port,
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36'
+        }
+      },
+        function(res){
+          var ip = res.socket.remoteAddress;
+          resolve(ip);
+        }
+      )
+    }catch(e){
+      reject(e);
+    };
   })
 };
