c26b6af29563ccfd4c2f558b96e8ab922c295e75
sueqk <68449029+k239i@users.noreply.github.com>
2021-03-26 19:46:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.on('message', async m => {
   if(!m.mentions.has(client.user, { ignoreEveryone: true, ignoreRoles: true})) return;
   var chal = !!(m.content.match('-ch_al'));
   var chpi = !!(m.content.match('-ch_pi'));
-  var _url = m.content.replace(/(<@!?\d+>\s?(\s?-ch_al|\s?-ch_pi)?(\s+)?)/,'');
+  var _url = m.content.replace(/(<@!?\d+>\s?(\s?-ch_al\s?|\s?-ch_pi\s?)+?(\s+)?)/,'');
   var host = new url.parse(_url);
   if(!m.content.match(/https?:\/\//)){
     var vaa = _url.split(':');
@@ -101,7 +101,7 @@ function ping(host){
       ahokusa += 1;
     });
     setTimeout(() => {
-      reject('timeouted!');
+      reject('timeouted! ' + ahokusa + 'ms');
     },10 * 1000);
     try{
       http.get({
